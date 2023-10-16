c8d100dc1ed2e07d9a4356b97985323054dd7be1
Kemi <G00379841@atu.ie>
2023-10-16 14:38:55 +0000

    lab4 complete

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,74 @@
+import { useState } from "react";
 
 {/*create application*/}
 
+
 function Create(){
+
+    const [title, setTitle] = useState('');
+    const [cover, setCover] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        console.log("Title "+title+
+        "Cover: "+cover+
+        "Author: "+author);
+
+    }
+
+
+{/*Modifed the Create component so that it includes a form that will upload data
+to a server.*/}
+
     return(
         <div>
             <h2>Hello from my Create component</h2>
+            <form onSubmit={handleSubmit}>
+
+
+                <div className="from-group">
+                    <label>Add Book Title</label>
+                    <input type="text"
+                    className="from-control"
+                    value={title}
+                    onChange={(e) => {setTitle(e.target.value)}}
+
+                    />
+                </div>
+
+                <div className="from-group">
+                    <label>Add Book Cover</label>
+                    <input type="text"
+                    className="from-control"
+                    value={cover}
+                    onChange={(e) => { setCover(e.target.value)}}
+                    
+                    />
+                </div>
+
+                <div className="from-group">
+                    <label>Add Book Author</label>
+                    <input type="text"
+                    className="from-control"
+                    value={author}
+                    onChange={(e) => {setAuthor(e.target.value)}}
+                    
+                    />
+                </div>
+<div>
+
+    {/*made a submit button*/}
+
+                <input
+                type="submit"
+                value="Create book">
+                </input>
+                </div>
+
+            </form>
+
         </div>
         );
     
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,45 +1,35 @@
+import { useEffect, useState } from 'react';
 import Books from "./Books";
+import axios from "axios";
 
 {/*Added a new component written as a function called books to our read component. */}
 
 {/*Added the some JSON to the read component and store it in a const variable. */}
 
 function Read(){
-    const data = [
-        {
-        "title": "Learn Git in a Month of Lunches",
-        "isbn": "1617292419",
-        "pageCount": 0,
-        "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-        "status": "MEAP",
-        "authors": ["Rick Umali"],
-        "categories": []
-        },
-        {
-        "title": "MongoDB in Action, Second Edition",
-        "isbn": "1617291609",
-        "pageCount": 0,
-        "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-        "status": "MEAP",
-        "authors": [
-        "Kyle Banker",
-        "Peter Bakkum",
-        "Tim Hawkins",
-        "Shaun Verch",
-        "Douglas Garrett"
-        ],
-        "categories": []
-        },
-        {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-            }
-            ];
+    const [data, setData] = useState([]);
+
+{/* Used the react hook, useEffect and synchronized a component with an external system.*/}
+
+{/* Made a http get call that will return the json data */}
+{/* Add axios to our project, axios is a Promise based HTTP client.*/}
+
+useEffect(()=>{
+
+    axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
+    .then(
+        (response)=>{
+            setData(response.data.books);
+        }
+    )
+    .catch(
+        (error)=>{
+            console.log(error);
+
+        }
+    );
+
+}, []);
 
 
 
